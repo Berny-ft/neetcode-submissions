0355eaa8653f91c3cee9ea88656002bfee732d9f
Berny <57468423+Berny-ft@users.noreply.github.com>
2026-04-10 21:42:42 -0400

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        # you do need to go through all the houses
+
+        rob1,rob2 = 0,0
+
+        for i in nums:
+            temp = max(i+rob1, rob2)
+            rob1 = rob2
+            rob2 = temp
+        return temp
