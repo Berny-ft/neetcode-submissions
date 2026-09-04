9669fb3364ee7573bed9000cdd3b2921593e034b
Berny <57468423+Berny-ft@users.noreply.github.com>
2026-09-05 06:48:55 +0900

    Add: evaluate-reverse-polish-notation - submission-19

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-19.py
@@ -0,0 +1,43 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        #stoare a stack
+
+        # store a current value 
+
+        # while tokens stack values 
+
+        # if value is operand pop the last two values and apply the operand
+
+ 
+
+        def comp(stack,c):
+
+            if c =="+":
+                a = stack.pop()
+                b = stack.pop()
+                return a+b
+            elif c == "-":
+                a = stack.pop()
+                b = stack.pop()
+                return b-a
+            elif c == "/":
+                a = stack.pop()
+                b = stack.pop()
+                if (a < 0) ^ (b < 0) : 
+                    return int(b/a)
+                return b//a
+            elif c =="*":
+                a = stack.pop()
+                b = stack.pop()
+                return b*a
+
+        ops = ("+","*","-","/")
+        stack = []
+
+        for val in tokens:
+            if val in ops:
+                stack.append(comp(stack, val))
+            else :
+                stack.append(int(val))
+
+        return stack[0]
